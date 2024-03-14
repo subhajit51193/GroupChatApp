472084e122327ea1c5e52ba23103d1bfbee2bb83
subhajit51193 <nnorth87@gmail.com>
2024-03-14 22:13:18 +0530

    basic function working

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,17 @@
 
 function showMessage(message){
 	
+	$("#message-container-table").prepend(`<tr><td><b>${message.name} :</b>${message.content}</td></tr>`)
+}
+
+function sendMessage(){
+	
+	let jsonOb ={
+		name:localStorage.getItem("name"),
+		content: $("#message-value").val()
+	}
+	
+	stompClient.send("/app/message",{},JSON.stringify(jsonOb));
 }
 
 
@@ -29,9 +40,28 @@ $(document).ready(e=>{
 	
 	$("#login").click(()=>{
 		
-		let name  = $("#name-value").val();
-		localStorage.setItem("name",name);
+		let name  = $("#name-value").val()
+		localStorage.setItem("name",name)
+		$("#name-title").html(`Welcome - <b>${name}</b>`)
 		
 		connect();
+	})
+	
+	
+	$("#send-btn").click(()=>{
+		
+		sendMessage();
+	})
+	
+	$("#logout").click(()=>{
+		
+		localStorage.removeItem("name")
+		if (stompClient !== null){
+			stompClient.disconnect();
+			$("#name-from").removeClass("d-none");
+			$("#chat-room").addClass("d-none");
+			console.log(stompClient);
+			
+		}
 	})
 })
